0a42c29464073f4a8aeaac3d5499448b6c50c244
Adam <adamwmcneil@gmail.com>
2023-11-26 14:31:38 -0600

    finished basic functionality

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
 function changeText() {
-    document.getElementById("greeting").innerHTML = "Super secret message ";
+    var today = new Date();
+    console.log("you need to click the button at midnight")
+    console.log(today.getMinutes())
+    console.log(today.getMinutes() % 2)
+    console.log(today.getHours())
+    var theRealBool = today.getHours() == 0 && today.getMinutes() < 10
+    var testBool = today.getMinutes() % 2 == 0
+    if (testBool) {
+        document.getElementById("greeting").innerHTML = "Super secret message";
+    }
+    else {
+        document.getElementById("greeting").innerHTML = "Hello, World!"
+    }
   }
