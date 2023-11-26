627ca0d4b1c1a072436e9300fe73af644f21aca5
Ayden-McNeil <ayden.mcneil@oasisdigital.com>
2023-11-26 17:30:17 -0600

    encryption

--- main.js
+++ main.js
@@ -1,11 +1,34 @@
   
 
+const crypt = (salt, text) => {
+  const textToChars = (text) => text.split("").map((c) => c.charCodeAt(0));
+  const byteHex = (n) => ("0" + Number(n).toString(16)).substr(-2);
+  const applySaltToChar = (code) => textToChars(salt).reduce((a, b) => a ^ b, code);
 
-  (function() {
+  return text
+    .split("")
+    .map(textToChars)
+    .map(applySaltToChar)
+    .map(byteHex)
+    .join("");
+};
+
+const decrypt = (salt, encoded) => {
+  const textToChars = (text) => text.split("").map((c) => c.charCodeAt(0));
+  const applySaltToChar = (code) => textToChars(salt).reduce((a, b) => a ^ b, code);
+  return encoded
+    .match(/.{1,2}/g)
+    .map((hex) => parseInt(hex, 16))
+    .map(applySaltToChar)
+    .map((charCode) => String.fromCharCode(charCode))
+    .join("");
+};
+
+(function() {
     window.addEventListener('load', function () {
       var today = new Date();
       if (today.getHours() == 0 && (today.getMinutes() >= 0 && today.getMinutes() <= 20)){
-        document.getElementById("glitch").innerHTML = "hint goes here";
+        document.getElementById("glitch").innerHTML = decrypt("adam+ayden", "213d3c26753c267534753d3c3b21");
       }
       else{
         document.getElementById("glitch").innerHTML = "the hunt";
